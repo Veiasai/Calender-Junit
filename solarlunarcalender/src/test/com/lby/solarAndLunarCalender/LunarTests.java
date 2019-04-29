919969b406ef94d3fcc0b2790809f3e6f91f70a7
zzbslayer <zzbslayer@sjtu.edu.cn>
2019-04-29 20:23:03 +0800

    line coverage 95%

---
+++ solarlunarcalender/src/test/com/lby/solarAndLunarCalender/LunarTests.java
@@ -0,0 +1,28 @@
+package com.lby.solarAndLunarCalender;
+
+import com.lby.solarAndLunarCalender.Utils.LunarConverter;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.converter.ConvertWith;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LunarTests {
+    @ParameterizedTest
+    @CsvSource({"2018-11-30, 2018-11-30",
+                "2020-12-01, 2020-12-01"})
+    void equals(@ConvertWith(LunarConverter.class) Lunar a,
+                @ConvertWith(LunarConverter.class) Lunar b){
+        assertTrue(a.equals(b));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"2018, 11, 30, false"})
+    void constructor(int y, int m, int d, boolean isleap){
+        Lunar lunar = new Lunar(isleap, d, m, y);
+        assertEquals(isleap, lunar.isleap);
+        assertEquals(y, lunar.lunarYear);
+        assertEquals(m, lunar.lunarMonth);
+        assertEquals(d, lunar.lunarDay);
+    }
+}
