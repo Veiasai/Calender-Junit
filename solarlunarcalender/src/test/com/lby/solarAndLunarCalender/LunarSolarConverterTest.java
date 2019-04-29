0a8718aecca3c37efc1cc37b346a49a43106b934
veia-mac <917971502@qq.com>
2019-04-29 08:14:01 +0800

    add junit5 param

--- solarlunarcalender/src/test/com/lby/solarAndLunarCalender/LunarSolarConverterTest.java
+++ solarlunarcalender/src/test/com/lby/solarAndLunarCalender/LunarSolarConverterTest.java
@@ -1,7 +1,11 @@
 package com.lby.solarAndLunarCalender;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,17 +18,28 @@ class LunarSolarConverterTest {
         lunarSolarConverter = new LunarSolarConverter();
     }
 
-    @Test
-    void lunarYearToGanZhi() {
-        String r = lunarSolarConverter.lunarYearToGanZhi(100);
+    @Nested
+    class LunarYearToGanZhi{
+        @ParameterizedTest
+        @CsvSource({"5,乙丑年", "8,戊辰年"})
+        void normal(int input, String output) {
+            String r = lunarSolarConverter.lunarYearToGanZhi(input);
 
-        // In a grouped assertion all assertions are executed, and any
-        // failures will be reported together.
-        assertEquals("Wrong", r);
+            // In a grouped assertion all assertions are executed, and any
+            // failures will be reported together.
+            assertEquals(output, r);
+        }
+
+        @ParameterizedTest
+        @ValueSource(ints = {0, 1, 2})
+        void outOfBound(int input) {
+            assertThrows(ArrayIndexOutOfBoundsException.class, ()->{lunarSolarConverter.lunarYearToGanZhi(input);});
+        }
     }
 
     @Test
     void lunarToSolar() {
+
     }
 
     @Test
